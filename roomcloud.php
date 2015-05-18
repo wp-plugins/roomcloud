46b0f26000ef3da2942b743b09d94eafbc4cd9f2
roomcloud <roomcloud@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 19:51:32 +0000

    working version2
    git-svn-id: https://plugins.svn.wordpress.org/roomcloud/tags/1.3@1162898 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- roomcloud.php
+++ roomcloud.php
@@ -3,7 +3,7 @@
  * Plugin Name: Roomcloud
  * Plugin URI: http://www.roomcloud.net
  * Description: A Plugin to add roomcloud booking form to hotel website using [roomcloud] shortcode
- * Version: 1.1
+ * Version: 1.3
  * Author: Raffaello Bindi
  * Author URI: http://www.roomcloud.net
  * License: GPL2
@@ -18,6 +18,7 @@ function e($val){
 function rc_iframe($atts){
 
 /*
+echo("");
 echo("hotel:");
 echo($_POST['hotel']);
 echo('<br>');
